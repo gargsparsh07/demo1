7850c67a688e1ee5e71f79dc622c05e89bdcbe9f
gargsparsh07 <sparshgarg.pks@gmail.com>
2026-01-23 11:42:59 +0530

    [sparsh]:feat(object): track only objectorientedprogramming package

---
+++ src/objectorientedprogramming/encapsulationpolymorphisminterfaceandabstractclass/HospitalManagementSystem.java
@@ -0,0 +1,28 @@
+package objectorientedprogramming.encapsulationpolymorphisminterfaceandabstractclass;
+interface MedicalRecord {
+    void addRecord(String record);
+    void viewRecords();
+}
+
+abstract class Patient {
+    protected String name;
+    public Patient(String name) { this.name = name; }
+    public abstract double calculateBill();
+}
+
+class InPatient extends Patient {
+    public InPatient(String n) { super(n); }
+    public double calculateBill() { return 5000; }
+}
+
+class OutPatient extends Patient {
+    public OutPatient(String n) { super(n); }
+    public double calculateBill() { return 800; }
+}
+
+public class HospitalManagementSystem {
+    public static void main(String[] args) {
+        Patient p = new InPatient("Ravi");
+        System.out.println("Bill: ₹" + p.calculateBill());
+    }
+}
